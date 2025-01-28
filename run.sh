448e7c5e06ffea1aa0cf00b834ed98aadf5c11d9
Eric Xiong <xiongxz@students.cs.ubc.ca>
2025-01-27 20:35:42 -0800

    Adjustment to A0-P0

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@
 #SBATCH --mem=8G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=xinze5@ualberta.ca
-#SBATCH --job-name=A1-P0-DQN
-#SBATCH --output=A1-P0-DQN.out
+#SBATCH --job-name=A1-P0-DQNAux
+#SBATCH --output=A1-P0-DQNAux.out
 
 module load apptainer
-apptainer exec -C -B .:${HOME} -W ${SLURM_TMPDIR} pyproject.sif ./scripts/A1-P0-DQN.sh
+apptainer exec -C -B .:${HOME} -W ${SLURM_TMPDIR} pyproject.sif ./scripts/A1-P0-DQNAux.sh
 
