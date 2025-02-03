bfaa67de46b9126e2acc43e16f08c0b76e800442
Anffany Chen <anffany@ualberta.ca>
2025-02-03 13:46:36 -0700

    test slurm scheduler

--- scripts/E1-P0.sh
+++ scripts/E1-P0.sh
@@ -3,4 +3,5 @@
 # exit script on error
 set -e
 
-python scripts/local.py --runs 5 -e experiments/Gridworld/E1/P0/DQN-Relu.json 
+#python scripts/local.py --runs 5 -e experiments/Gridworld/E1/P0/DQN-Relu.json 
+python scripts/slurm.py --cluster clusters/cedar.json --runs 5 -e experiments/Gridworld/E1/P0/DQN-Relu.json 
