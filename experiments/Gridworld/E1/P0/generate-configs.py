d9ef19a576caf7e77a14aaece410ee40a8c3d40d
Anffany Chen <anffany@ualberta.ca>
2025-02-12 13:17:12 -0700

    set up sweep config for Scratch-FTA

--- experiments/Gridworld/E1/P0/generate-configs.py
+++ experiments/Gridworld/E1/P0/generate-configs.py
@@ -40,17 +40,55 @@ ReLU_template = {
             "frozen": False
         },
         "environment": {
-            "goal_id": ["-1"]
+            "goal_id": "0"
         }                
     }
 }
 
+FTA_template = {
+    "agent": "DQN-FTA",
+    "problem": "Gridworld",
+    "total_steps": 100000,
+    "episode_cutoff": 100,
+    "early_saving": -1,
+    "metaParameters": {
+        "experiment": {
+            "load": False,
+            "save": False,
+            "seed_offset": 0
+        },
+        "epsilon": 0.1,
+        "target_refresh": 64,
+        "buffer_type": "uniform",
+        "buffer_size": 10000,
+        "batch": 32,
+        "n_step": 1,
+        "normalizer": {
+            "state": {
+                "coeff": 255.0
+            }
+        },
+        "optimizer": {
+            "name": "ADAM",
+            "alpha": [0.001, 0.0003, 0.0001, 0.00003, 0.00001],
+            "beta1": 0.9,
+            "beta2": 0.999
+        },
+        "representation": {
+            "type": "MazeNetFTA",
+            "hidden": 32,
+            "eta": 0.2,
+            "frozen": False
+        },
+        "environment": {
+            "goal_id": "0"
+        }
+    }
+}
 
 #%%
 gridworld_scratch_path = Path(os.getcwd() + "/experiments/Gridworld/E1/P0/best-ScratchRelu")
 gridworld_scratch_path.mkdir(exist_ok=True)
-
-#%%
 
 old_best_alpha = {'-1': 0.0001, '104': 0.0001, '109': 0.0003, '114': 0.0001, '119': 0.0001, '124': 0.0001, '129': 0.0001, '134': 3e-05, '139': 0.0001, '14': 0.0001, '144': 0.0001, '149': 0.0001, '154': 0.0001, '159': 0.0001, '164': 0.0001, '169': 0.0001, '19': 0.0001, '24': 0.0001, '29': 0.0001, '34': 0.0001, '39': 0.0001, '4': 0.0001, '44': 0.0001, '49': 0.0003, '54': 0.0003, '59': 0.0001, '64': 0.0001, '69': 0.0003, '74': 0.0001, '79': 0.0003, '84': 0.0001, '89': 0.0001, '9': 0.0001, '94': 0.0001, '99': 0.0001}
 best_alpha = {f'{int(key)+1}': old_best_alpha[key] for key in old_best_alpha.keys()}
@@ -67,6 +105,14 @@ for goal, alpha in best_alpha.items():
     gridworld_scratch_json_paths.append(gridworld_scratch_json_path)
     with open(gridworld_scratch_json_path, "w") as f:
         gridworld_scratch_best = json.dump(gridworld_scratch, f, indent=4)
+
+#%%
+sample_goal = sorted([int(key) for key in best_alpha.keys()])
+scratch_FTA_sweep = deepcopy(FTA_template)
+scratch_FTA_sweep["agent"] += "-sweep"
+scratch_FTA_sweep["metaParameters"]["environment"]["goal_id"] = [f'{id}' for id in sample_goal]
+with open(os.getcwd() +  "/experiments/Gridworld/E1/P0/DQN-FTA-sweep-new.json", "w") as f:
+    json.dump(scratch_FTA_sweep, f, indent=4)
 
 # %%
 script_name = os.getcwd() + "/scripts/best-ScratchRelu.sh"
