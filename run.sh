0936f81dbce57fef05227276fa1ce976e18279a7
Eric Xiong <xiongxz@students.cs.ubc.ca>
2025-04-14 13:26:50 -0700

    A2-P0: Change experiments

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@
 #SBATCH --account=rrg-whitem
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=xinze5@ualberta.ca
-#SBATCH --job-name=A2-P0-DQN
-#SBATCH --output=A2-P0-DQN-%j.out
+#SBATCH --job-name=A2-P0-32
+#SBATCH --output=A2-P0-32-%j.out
 
 export OMP_NUM_THREADS=1
 
 module load apptainer
-apptainer exec -C -B .:${HOME} -W ${SLURM_TMPDIR} pyproject.sif ./scripts/A2-P0-DQN.sh
+apptainer exec -C -B .:${HOME} -W ${SLURM_TMPDIR} pyproject.sif ./scripts/A2-P0-32.sh
 
