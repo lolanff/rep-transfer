c53b18c9befb79b17623bf14ea9d804499cdd04d
Eric Xiong <xiongxz@students.cs.ubc.ca>
2025-04-16 13:21:15 -0700

    A2-P2: Revised experiments to compare trade-offs in T-maze

---
+++ scripts/A2-P2-fta-all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+python scripts/local.py --runs 5 -e experiments/Gridworld/A2/P2/t_maze_drqn_fta_use_all_steps_hyper_sweep/TMaze-DRQN-fta-use-all-steps-hyper-sweep.json
---
+++ scripts/A2-P2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+python scripts/local.py --runs 5 -e experiments/Gridworld/A2/P2/t_maze_drqn_hyper_sweep/TMaze-DRQN-hyper-sweep.json
